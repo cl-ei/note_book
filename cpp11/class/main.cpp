8561e4cf589f6159161bdddbc8f14f6504d2afc8
Caoliang <i@caoliang.net>
2018-01-27 14:15:18 +0800

    dynimic mem

--- cpp11/class/main.cpp
+++ cpp11/class/main.cpp
@@ -12,10 +12,34 @@ int count_numbers(int x, ...){
     va_start(args_pointer, x);
     cout << *va_arg(args_pointer, char*) << endl;
     va_end(args_pointer);
+    return 0;
 }
 
 void malloc_test(){
+    int ** d2;
+    d2 = (int **)malloc(sizeof(int *)*2);
+    cout << "d2: " << d2 << endl;
 
+    int * temp;
+    for (int i = 0; i < 2; i++){
+        temp = (int*)malloc(sizeof(int)*2);
+        cout << "i: " << i << ", temp: " << temp << endl;
+
+        d2[i] = temp;
+    }
+    d2[1][1] = 9;
+
+    cout << "addr d[0]: " << &d2[1] << endl
+        << sizeof(int **) << endl;
+
+    cout << d2[1][1] << " - " << &d2[1][1] << endl;
+
+    // cpp11
+    auto cpp_d2 = new int[2][3];
+    cout << "typeof cpp_d2: " << typeid(cpp_d2).name() << endl;
+
+    int (*cpp_d22)[3] = new int[2][3];
+    cout << "typeof cpp_d22: " << typeid(cpp_d22).name() << endl;
 }
 
 int main() {
