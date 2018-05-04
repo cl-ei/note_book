0a451c35d769b8b1fb671c9ec3f5c7c052f24b62
Caoliang <i@caoliang.net>
2018-05-04 09:43:33 +0800

    reset proj.

--- utils/p_a_1.py
+++
@@ -1,4 +0,0 @@
-from prize_accepter import PrizeAccepter
-
-if __name__ == "__main__":
-    PrizeAccepter(loger_name="prize_1", log_file_path="prize_1.log").run()
--- utils/run_redis.vbs
+++
@@ -1,2 +0,0 @@
-set ws=wscript.createobject("WScript.Shell")
-ws.run "C:\\Backup\\redis-64.3.0.503\\redis-server.exe", 0
